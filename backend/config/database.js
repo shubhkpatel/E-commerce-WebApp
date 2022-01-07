3c7c48233321812eb01736ac5d03f9069de0ada5
Shubh <patelshubh694@gmail.com>
2022-01-07 16:59:15 +0530

    Added Middlewares for Backend Error Handling

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,9 +4,7 @@ const connectDatabase = () => {
     mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
         .then((data) => {
             console.log(`MongoDB connected with server: ${data.connection.host}`);
-        }).catch((err) => {
-            console.error(err);
-        })
+        });
 };
 
 module.exports = connectDatabase;
