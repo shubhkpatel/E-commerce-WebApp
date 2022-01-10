654ddf8542fdd9b6665d8f39ba7cafaf484e854c
Shubh <patelshubh694@gmail.com>
2022-01-10 18:26:13 +0530

    Created Route for Logout and Added Authentication & Roles based authorization for Security

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require('../controllers/productController');
 const router = express.Router();
+const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
 
 
-router.route("/products").get(getAllProducts);
-router.route("/product/new").post(createProduct);
-router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails);
+router.route("/products").get(isAuthenticatedUser, getAllProducts);
+router.route("/product/new").post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
+router.route("/product/:id")
+    .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+    .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct)
+    .get(getProductDetails);
 
 
 module.exports = router;
