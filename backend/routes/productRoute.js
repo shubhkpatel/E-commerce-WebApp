6e1591384f87f3b12a48b911cf648c389ab78c91
Shubh <patelshubh694@gmail.com>
2022-01-11 16:48:03 +0530

    Added Forget Password and Reset Password using NodeMailer

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
 
 
-router.route("/products").get(isAuthenticatedUser, getAllProducts);
+router.route("/products").get(getAllProducts);
 router.route("/product/new").post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
 router.route("/product/:id")
     .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
