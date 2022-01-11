34ee1785761dd994c9d4e88c500d6f5632405d22
Shubh <patelshubh694@gmail.com>
2022-01-11 16:56:53 +0530

    ErrorHandling for Duplicate Registeration & Invalid JWT Token or Expired JWT Token

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -10,6 +10,24 @@ module.exports = (err, req, res, next) => {
         err = new ErrorHandler(message, 400);
     }
 
+    //Mongoose Duplicate Key Error
+    if (err.code === 11000) {
+        const message = `Duplicate ${Object.keys(err.keyValue)} Entered`
+        err = new ErrorHandler(message, 400);
+    }
+
+    //Wrong JWT error
+    if (err.name === "JsonWebTokenError") {
+        const message = `Json Web Token is Invalid, try again`
+        err = new ErrorHandler(message, 400);
+    }
+
+    //JWT expire error
+    if (err.name === "TokenExpireError") {
+        const message = `Json Web Token is Expired, try again`
+        err = new ErrorHandler(message, 400);
+    }
+
     res.status(err.statusCode).json({
         success: false,
         message: err.message
