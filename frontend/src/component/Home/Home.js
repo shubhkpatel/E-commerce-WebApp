8a7ab8a68a726d3cfade832ac2d19fce550f34dd
Shubh <patelshubh694@gmail.com>
2022-01-16 10:08:59 +0530

    Implemented Redux & Fetched Products and Rendered on HomePage. Displayed Loading Screen while products are being fetched. Display Errors on Bottom-Center if any

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,42 +1,52 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { CgMouse } from "react-icons/all";
 import "./Home.css";
 import ProductCard from "./ProductCard";
-
-const product = {
-    name: "Blue Shirt",
-    images: [{ url: "http://i.ibb.co/DRST11n/1.webp" }],
-    price: "3000",
-    _id: "shubh_18"
-}
+import MetaData from "../layout/MetaData";
+import { getProduct } from "../../actions/productAction";
+import { useSelector, useDispatch } from "react-redux";
+import Loader from "../layout/Loader/Loader";
+import { useAlert } from "react-alert";
 
 const Home = () => {
+
+    const alert = useAlert();
+    const dispatch = useDispatch();
+    const { loading, error, products, productsCount } = useSelector((state) => state.products);
+
+    useEffect(() => {
+        if(error){
+            return alert.error(error);
+        }
+        dispatch(getProduct());
+    }, [dispatch, error]);
+
     return (
         <Fragment>
-            <div className="banner">
-                <p>Welcome to Ecommerce</p>
-                <h1>FIND AMAZING PRODUCTS BELOW</h1>
+            {loading ? (<Loader />) : (
+                <Fragment>
+                    <MetaData title="ECOMMERCE" />
 
-                <a href="#container">
-                    <button>
-                        Scroll <CgMouse />
-                    </button>
-                </a>
-            </div>
+                    <div className="banner">
+                        <p>Welcome to Ecommerce</p>
+                        <h1>FIND AMAZING PRODUCTS BELOW</h1>
 
-            <h2 className="homeHeading">Featured Products</h2>
+                        <a href="#container">
+                            <button>
+                                Scroll <CgMouse />
+                            </button>
+                        </a>
+                    </div>
 
-            <div className="container" id="container">
-                <ProductCard product={product} />
-                <ProductCard product={product} />
-                <ProductCard product={product} />
-                <ProductCard product={product} />
-                
-                <ProductCard product={product} />
-                <ProductCard product={product} />
-                <ProductCard product={product} />
-                <ProductCard product={product} />
-            </div>
+                    <h2 className="homeHeading">Featured Products</h2>
+
+                    <div className="container" id="container">
+                        {products && products.map(product => (
+                            <ProductCard product={product} />
+                        ))}
+                    </div>
+                </Fragment>
+            )}
         </Fragment>
     );
 };
--- frontend/src/component/Home/ProductCard.js
+++ frontend/src/component/Home/ProductCard.js
@@ -7,19 +7,19 @@ const ProductCard = ({ product }) => {
         color: "rgba(20, 20, 20, 0.1)",
         activeColor: "tomato",
         size: window.innerWidth < 600 ? 20 : 25,
-        value: 2.5,
+        value: product.ratings,
         isHalf: true
     };
 
     return (
-        <Link className="productCard" to={product.id}>
+        <Link className="productCard" to={product._id}>
             <img src={product.images[0].url} alt={product.name} />
             <p>{product.name}</p>
             <div>
                 <ReactStars {...options} />{" "}
                 <span className="productCardSpan">
                     {" "}
-                    (256 Reviews)
+                    ({product.numOfReviews} Reviews)
                 </span>
             </div>
             <span>{`₹${product.price}`}</span>
