31923cc3ba97a3676a170e1db5b42116fd137c3b
Shubh <patelshubh694@gmail.com>
2022-01-23 12:08:04 +0530

    Designed Cart Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,6 +19,7 @@ import UpdateProfile from './component/User/UpdateProfile';
 import UpdatePassword from './component/User/UpdatePassword';
 import ForgotPassword from './component/User/ForgotPassword';
 import ResetPassword from './component/User/ResetPassword';
+import Cart from './component/Cart/Cart';
 
 function App() {
   const { isAuthenticated, user } = useSelector((state) => state.user);
@@ -50,6 +51,7 @@ function App() {
         <ProtectedRoute exact path="/password/update" component={UpdatePassword} />
         <Route exact path="/password/forgot" component={ForgotPassword} />
         <Route exact path="/password/reset/:token" component={ResetPassword} />
+        <Route exact path="/cart" component={Cart} />
       </Switch>
       <Footer />
     </Router>
--- frontend/src/component/layout/Header/UserOptions.js
+++ frontend/src/component/layout/Header/UserOptions.js
@@ -13,7 +13,7 @@ import { logout } from "../../../actions/userAction";
 import { useDispatch, useSelector } from "react-redux";
 
 const UserOptions = ({ user }) => {
-    // const { cartItems } = useSelector((state) => state.cart);
+    const { cartItems } = useSelector((state) => state.cart);
 
     const [open, setOpen] = useState(false);
     const history = useHistory();
@@ -23,15 +23,15 @@ const UserOptions = ({ user }) => {
     const options = [
         { icon: <ListAltIcon />, name: "Orders", func: orders },
         { icon: <PersonIcon />, name: "Profile", func: account },
-        // {
-        //     icon: (
-        //         <ShoppingCartIcon
-        //             style={{ color: cartItems.length > 0 ? "tomato" : "unset" }}
-        //         />
-        //     ),
-        //     name: `Cart(${cartItems.length})`,
-        //     func: cart,
-        // },
+        {
+            icon: (
+                <ShoppingCartIcon
+                    style={{ color: cartItems.length > 0 ? "tomato" : "unset" }}
+                />
+            ),
+            name: `Cart(${cartItems.length})`,
+            func: cart,
+        },
         { icon: <ExitToAppIcon />, name: "Logout", func: logoutUser },
     ];
 
@@ -55,12 +55,13 @@ const UserOptions = ({ user }) => {
         history.push("/account");
     }
 
-    // function cart() {
-    //     history.push("/cart");
-    // }
+    function cart() {
+        history.push("/cart");
+    }
 
     function logoutUser() {
         dispatch(logout());
+        history.push("/login");
         alert.success("Logout Successfully");
     }
 
@@ -89,7 +90,7 @@ const UserOptions = ({ user }) => {
                         icon={item.icon}
                         tooltipTitle={item.name}
                         onClick={item.func}
-                        // tooltipOpen={window.innerWidth <= 600 ? true : false}
+                        tooltipOpen={window.innerWidth <= 600 ? true : false}
                     />
                 ))}
             </SpeedDial>
